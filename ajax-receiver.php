7a3ccd1844564a5f8411e3d890a1f28f374ee03e
rektph <kendrickjaviercosca@gmail.com>
2017-08-19 16:28:43 +0800

    Add files via upload

--- ajax-receiver.php
+++ ajax-receiver.php
@@ -1,6 +1,11 @@
 <?php
 	$text = explode(" ", strtolower($_POST['textValue']));
 	sort($text);
+	$count = array_count_values($text);
 	$text = array_unique($text);
-	$text = implode("<br>", $text);
-	echo json_encode(array('revstring' => $text));
+	$ans = array();
+	foreach($text as $key => $value) {
+		array_push($ans, "$value - ". $count["$value"] );
+	}
+	$ans = implode("<br>", $ans);
+	echo json_encode(array('answer' => $ans));
--- index.php
+++ index.php
@@ -1,13 +1,21 @@
 <html>
 <head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 	<title>Javascript Review</title>
-	<script 
-		src="http://code.jquery.com/jquery-3.2.1.min.js"></script>
-
+	<script src="http://code.jquery.com/jquery-3.2.1.min.js"></script>
 </head>
-<body>
-	<div id="content"></div>
-	<input type="text" id="textinput" />
+<body class="container-fluid">
+<br>
+	<div class="row">
+		<div class="col-md-3"></div>
+		<input type="text" class="form-control col-md-6" id="textinput" />
+		<div class="col-md-3"></div>
+	</div>
+	<div class="row">
+		<div class="col-md-3"></div>
+		<div id="content" class="col-md-6"></div>
+		<div class="col-md-3"></div>
+	</div>
 </body>
 <script	type="text/javascript">
 	var jTextInput = $("#textinput");
@@ -20,7 +28,7 @@
 				"textValue": $("#textinput").val()
 			},
 			success: function(res) {
-				$("#content").html(res.revstring);
+				$("#content").html(res.answer);
 				console.log(res);
 			}
 		});
